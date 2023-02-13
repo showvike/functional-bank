81b3fca8d1909ba5e42164cc7037b2d86dfc0f59
showvike <showvike@gmail.com>
2023-02-13 17:39:21 +0600

    functional deposit done

--- js/deposit.js
+++ js/deposit.js
@@ -1,3 +1,18 @@
+// deposit button's event listener
 document.getElementById("btn-deposit").addEventListener("click", function () {
-    console.log("deposit");
+    // get new deposit amount
+    const newDepositAmount = getInputFieldValueById("deposit-field");
+    // get previous deposit amount
+    const previousDepositAmount = getElementValueById("deposit-total");
+    // add them
+    const totalDepositAmount = newDepositAmount + previousDepositAmount;
+    // set the total deposit amount
+    setElementValueById("deposit-total", totalDepositAmount);
+
+    // get previous balance
+    const previousBalanceAmount = getElementValueById("balance-total");
+    // add previous balance and new deposit amount
+    const totalBalanceAmount = previousBalanceAmount + newDepositAmount;
+    // add them
+    setElementValueById("balance-total", totalBalanceAmount);
 });
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,28 @@
-console.log("from utilities");
+function getInputFieldValueById(inputId) {
+    /*
+        get the input field value with the id
+    */
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = "";
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId) {
+    /*
+        get the element value with the id
+    */
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const elementValue = parseFloat(elementValueString);
+    return elementValue;
+}
+
+function setElementValueById(elementId, value) {
+    /*
+        set the element value with the id and value
+    */
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}

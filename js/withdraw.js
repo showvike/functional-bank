26688280be8608b9ce3a404560ef1b3c80860a16
showvike <showvike@gmail.com>
2023-02-14 21:59:36 +0600

    functional withdraw done

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,18 @@
+// withdraw button's event listener
+document.getElementById("btn-withdraw").addEventListener("click", function () {
+    // get new withdraw amount
+    const newWithdrawAmount = getInputFieldValueById("withdraw-field");
+    // get previous withdraw amount
+    const previousWithdrawAmount = getElementValueById("withdraw-total");
+    // add them
+    const totalWithdrawAmount = newWithdrawAmount + previousWithdrawAmount;
+    // set the total withdraw amount
+    setElementValueById("withdraw-total", totalWithdrawAmount);
 
+    // get previous balance
+    const previousBalanceAmount = getElementValueById("balance-total");
+    // add previous balance and new withdraw amount
+    const totalBalanceAmount = previousBalanceAmount - newWithdrawAmount;
+    // add them
+    setElementValueById("balance-total", totalBalanceAmount);
+});
